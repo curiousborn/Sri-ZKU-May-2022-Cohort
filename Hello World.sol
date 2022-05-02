f222abadb3480e5dcbd22f1a81bf124c1cf6c1b0
curiousborn <49113882+curiousborn@users.noreply.github.com>
2022-05-02 16:17:10 +0530

    Update Hello World.sol

--- Hello World.sol
+++ Hello World.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.6.0;
 
 contract HelloWorld{
+    //Variable to store the number
     uint256 public myNumber;
-
+    
+    // Set the value of myNumber
     function storeNumber(uint256 _myNumber) public {
         myNumber = _myNumber;
     }
-
+    
+    // Get the value of myNumber
     function retrieveNumber() public view returns (uint256){
         return myNumber;
     }
